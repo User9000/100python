3695e10e08e9ad194696271701293aadec67bdf8
Carlo Lam <calam12@gmail.com>
2017-06-19 22:11:01 -0700

    any function in python
    any function in python

---
+++ anyFunction.py
@@ -0,0 +1,17 @@
+#test the any function
+
+
+list1  = ['0','1','2','l']
+
+
+if any(i=='2' for i in list1):
+      print("found the number 2 in list")
+	  
+
+
+	  
+lst = [-1, True, "X", 0.00001]
+
+print (any(lst))
+ 
+ 
