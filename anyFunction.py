60efb0b8c63e21a537c38fd20e446aa8046ad8d7
Carlo Lam <Carlo@Carlos-MacBook-Pro.local>
2017-06-22 12:54:00 -0700

    update to any function
    update to any function

--- anyFunction.py
+++ anyFunction.py
@@ -4,8 +4,8 @@
 list1  = ['0','1','2','l']
 
 
-if any(i=='2' for i in list1):
-      print("found the number 2 in list")
+if any(i=='2' for i in list1) and  any(i=='1' for i in list1):
+      print("found the number 2 and 1 are in list")
 	  
 
 
@@ -13,5 +13,8 @@ if any(i=='2' for i in list1):
 lst = [-1, True, "X", 0.00001]
 
 print (any(lst))
+
+
+
  
  
