cab882978cc0381bc039b52c29a79a7ed3cbfbaf
Carlo Lam <Carlo@Carlos-MacBook-Pro.local>
2017-06-29 22:06:49 -0700

    script 88
    script88 and script 88b

--- ex88.py
+++ ex88.py
@@ -2,14 +2,9 @@
 import pandas
 
 df = pandas.read_csv("countries-by-area.txt")
-
-
 df1= df['population_2013']/df["area_sqkm"]
-
-
 dfinal = df1.nlargest(5)
-#print(df1.nlargest(5))
-#print(df1)
+
 for i in dfinal.index:
     print(df["country"].iloc[i])
 
